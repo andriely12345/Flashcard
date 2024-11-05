7514a0f690c254a2e168472516094ae4dd7efe3b
andriely12345 <151225378+andriely12345@users.noreply.github.com>
2024-11-05 08:53:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'Free Fire',
+    'Em que ano foi lançado o FF?',
+    '2017'
+)
+
+criaCartao(
+    'Free Fire',
+    'Qual foi o personagem mais usado?',
+    'Hayato'
+)
+
+criaCartao(
+    'Free Fire',
+    'Em que ano o FF bombou?',
+    '2019'
+    
+)
+
+criaCartao(
+    'Free Fire',
+    'Qual fori a arma mais usada?',
+    'Vector'
+    )
+
+ criaCartao(
+    'Free Fire',
+    'Quantos mapas o FF tem?',
+    '3'
+
+        )
+
+        
+criaCartao(
+    'Free Fire',
+    'Qual a patente mais alta do FF?',
+    'Global'
+    
+            )
+
+
+
