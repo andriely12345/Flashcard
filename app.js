fe3f1888aee79bde94c3ecb9696c6f619ceb55f7
andriely12345 <151225378+andriely12345@users.noreply.github.com>
2024-11-12 10:38:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.innerHTML = `<div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-        <p>O${pergunta}</p>
+        <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
     </div>
 </div>
     `
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+
+    cartao.addEventListener('click', viraCartao)
+
     container.appendChild(cartao)
 }
